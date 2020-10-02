16ee7f41c4ef08cd270c4252baa6dbc29034cdee
Raudel Valdes <raudel.valdes2015@gmail.com>
2020-10-01 20:03:55 -0400

    got the 3d scatter plot showing up

--- scatterPlot.py
+++ scatterPlot.py
@@ -1,6 +1,9 @@
-import plotly.graph_objects as go
 import plotly.io as pio
+import plotly.express as px
 
-fig = go.Figure(go.Scatter(x=[1, 2, 3, 4], y=[4, 3, 2, 1]))
+
+df = px.data.iris()
+fig = px.scatter_3d(df, x='sepal_length', y='sepal_width', z='petal_width',
+              color='species')
 fig.update_layout(title_text='hello world')
-pio.write_html(fig, file='hello_world.html', auto_open=True)
+pio.write_html(fig, file='./drone_simulation.html', auto_open=True)
